950b869387d34abee4137d47898fcab3b13e65f6
hitochix06 <pachara.philibert@hotmail.fr>
2023-04-06 16:30:09 +0200

    changement  couleur text

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 
 
 const Upload = styled.div`
+margin: 16px;
 min-height: 100px;
 width: 50%;
 border: 1px solid var(--background-darker);
 border-radius: 0.5rem;
 cursor: pointer;
 background-color: var(--background-secondary);
-color: white;
+color: rgba(184, 185, 191, 1);
 display: flex;
 align-items: center;
 justify-content: center;
