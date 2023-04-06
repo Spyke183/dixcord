87966a0511b6b8f2480905703f86149eada4b92c
hitochix06 <pachara.philibert@hotmail.fr>
2023-04-06 16:42:03 +0200

    changement code couleur

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -10,7 +10,7 @@ border: 1px solid var(--background-darker);
 border-radius: 0.5rem;
 cursor: pointer;
 background-color: var(--background-secondary);
-color: rgba(184, 185, 191, 1);
+color: var(--media-text);
 display: flex;
 align-items: center;
 justify-content: center;
