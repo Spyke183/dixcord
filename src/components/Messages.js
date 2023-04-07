3b5f929efc18f0dc04fd83173dffad9b824bea48
Spyke183 <tinorompaul@gmail.com>
2023-04-07 12:59:11 +0200

    CentrerLeLogo

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import discordLogo from "../images/Avatar.svg";
 import { useState } from "react";
 import msg from "../data/messages.json";
 
@@ -11,11 +10,7 @@ const Messages = styled.div`
 `;
 
 const InputContainer = styled.div`
-  position: relative;
-  align-items: center;
-  diplay: flex;
-
-  > img {
+  img {
     height: 40px;
     width: 40px;
     margin-right: 16px;
@@ -23,21 +18,21 @@ const InputContainer = styled.div`
 `;
 
 const ContainerGeneral = styled.div`
-  position: relative;
   display: flex;
   flex-direction: column;
 
-  > img {
+  img {
     height: 40px;
     width: 40px;
     margin-right: 16px;
   }
-  > span {
+  span {
     font-size: 15px;
     font-weight: 700;
     color: white;
   }
 `;
+
 const BarreContainer = styled.div`
   display: flex;
   padding-top: 1rem;
@@ -46,25 +41,28 @@ const BarreContainer = styled.div`
     background-color: var(--background-message-hover);
   }
 `;
+
 const NomContainer = styled.div`
-  > span {
+  span {
     font-size: 15px;
-    font-weight: 300;
-    color: white;
+    font-weight: 700;
+    color: var(--text);
   }
 `;
+
 const MessageContainer = styled.div`
-  > span {
+  span {
     font-size: 15px;
     font-weight: 300;
-    color: white;
+    color: var(--text);
   }
 `;
 
 const Upload = styled.div`
   margin: 16px;
   min-height: 100px;
-  width: 50%;
+  max-width: 100%;
+  width: 20rem;
   border: 1px solid var(--background-darker);
   border-radius: 0.5rem;
   cursor: pointer;
@@ -73,6 +71,10 @@ const Upload = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+
+  @media (max-width: 768px) {
+    width: 10rem;
+  }
 `;
 
 const RowContainer = styled.div`
@@ -98,6 +100,7 @@ export default function () {
                 </NomContainer>
                 <MessageContainer>
                   <span>{msg.message}</span>
+                  {msg.user === "Torea" && <Upload>file.png</Upload>}
                 </MessageContainer>
               </RowContainer>
             </BarreContainer>
