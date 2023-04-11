5d8f62dfc8c0500561ba562fd82cf1bf9fafd2bc
Spyke183 <114151896+Spyke183@users.noreply.github.com>
2023-04-11 21:06:48 +0200

    version-final

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -3,18 +3,8 @@ import { useState } from "react";
 import msg from "../data/messages.json";
 
 const Messages = styled.div`
-  grid-template-columns: 3rem 1fr;
-  margin-top: 1rem;
-  gap: 1rem;
-  padding: 0.25rem 1rem;
-`;
 
-const InputContainer = styled.div`
-  img {
-    height: 40px;
-    width: 40px;
-    margin-right: 16px;
-  }
+  padding: 0.25rem 1rem;
 `;
 
 const ContainerGeneral = styled.div`
@@ -42,6 +32,24 @@ const BarreContainer = styled.div`
   }
 `;
 
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center; /* ajout */
+  
+  img {
+    height: 40px;
+    width: 40px;
+    margin-right: 16px;
+    /*justify-self: center;*/ /* supprimé */
+  }
+`;
+
+const RowContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
 const NomContainer = styled.div`
   span {
     font-size: 15px;
@@ -59,10 +67,10 @@ const MessageContainer = styled.div`
 `;
 
 const Upload = styled.div`
-  margin: 16px;
+  margin: 0 16px 16px 0;
   min-height: 100px;
   max-width: 100%;
-  width: 20rem;
+  width: 47rem;
   border: 1px solid var(--background-darker);
   border-radius: 0.5rem;
   cursor: pointer;
@@ -77,10 +85,7 @@ const Upload = styled.div`
   }
 `;
 
-const RowContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
+
 
 export default function () {
   const [result, setResult] = useState(msg);
@@ -100,14 +105,13 @@ export default function () {
                 </NomContainer>
                 <MessageContainer>
                   <span>{msg.message}</span>
-                  {msg.user === "Torea" && <Upload>file.png</Upload>}
+                  {msg.media === "file.png" && <Upload>file.png</Upload>}
                 </MessageContainer>
               </RowContainer>
             </BarreContainer>
           ))}
         </ContainerGeneral>
       </Messages>
-      {/* //regarder le box-sizing https://developer.mozilla.org/fr/docs/Web/CSS/box-sizing */}
     </>
   );
 }
